f6c025fe47c0da339215cf6ed3f97d4519403aeb
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-11-11 01:15:39 -0300

    fix hydratation issue

--- app/hooks/use-is-desktop.tsx
+++ app/hooks/use-is-desktop.tsx
@@ -1,6 +1,9 @@
 import { useWindowSize } from "@uidotdev/usehooks";
 
 export function useIsDesktop() {
+  if (typeof document === "undefined") {
+    return false;
+  }
   const { width } = useWindowSize();
   if (width === null) {
     return false;
