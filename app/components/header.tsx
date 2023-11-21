7688e536156eb8fc3f5dad058596258dc4bec4ef
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-11-21 11:49:36 -0300

    add label for mobile

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -78,6 +78,7 @@ export function Header() {
                 to="/settings"
                 icon={faCog}
                 onClick={closeMenu}
+                label={isDesktop ? undefined : translate("HeaderSettingsLabel")}
               />
               {user === undefined
                 ? (
