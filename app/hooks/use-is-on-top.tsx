19b929969a1772e0f80fb181583613511ab8a21f
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-12-03 20:45:37 -0300

    refactor(hook): reuse existing hooks

--- app/hooks/use-is-on-top.tsx
+++ app/hooks/use-is-on-top.tsx
@@ -3,19 +3,19 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { useWindowScroll } from "@uidotdev/usehooks";
 import { useEffect, useState } from "react";
 
 export function useIsOnTop() {
-  const [isSticky, setIsSticky] = useState(false);
   if (typeof document === "undefined") {
     return false;
   }
+  const [isOnTop, setIsOnTop] = useState(false);
+  const [{ y }] = useWindowScroll();
+
   useEffect(() => {
-    function handleScroll() {
-      setIsSticky(window.scrollY === 0);
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  });
-  return isSticky;
+    setIsOnTop(y === 0);
+  }, [y]);
+
+  return isOnTop;
 }
