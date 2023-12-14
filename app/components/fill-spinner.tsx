595a06e24593878c0e84ddb421296dc23afb0608
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-12-14 19:40:54 -0300

    refactor(case-opening): tweak fill spinner

--- app/components/fill-spinner.tsx
+++ app/components/fill-spinner.tsx
@@ -4,7 +4,7 @@ export function FillSpinner({ className }: { className?: string }) {
   return (
     <div
       className={clsx(
-        "relative h-[32px] w-[32px] rotate-45 rounded-[50%] border-[4px] border-transparent before:absolute before:inset-[-4px] before:animate-[fill-spinner_1s_infinite_linear] before:rounded-[50%] before:border-[4px] before:border-white before:content-['']",
+        "relative h-[24px] w-[24px] rounded-[50%] border-[4px] border-transparent before:absolute before:inset-[-4px] before:animate-[fill-spinner_1s_infinite_linear] before:rounded-[50%] before:border-[4px] before:border-white before:content-['']",
         className
       )}
     />
