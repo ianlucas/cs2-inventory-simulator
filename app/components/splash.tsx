f4dd727a580268da8df18265be45c12c7ca036f1
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-03-13 11:14:27 -0300

    feat: add check for document

--- app/components/splash.tsx
+++ app/components/splash.tsx
@@ -56,40 +56,42 @@ export function Splash() {
       <script
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
-          __html: `const splash = document.getElementById("splash");
-const progress = document.getElementById("splash-progress");
-let n = 0;
-let loaded = false;
+          __html: `if (typeof document !== "undefined") {
+  const splash = document.getElementById("splash");
+  const progress = document.getElementById("splash-progress");
+  let n = 0;
+  let loaded = false;
 
-if (document.readyState === 'complete') {
-  loaded = true;
-} else {
-  window.addEventListener('load', () => {
+  if (document.readyState === 'complete') {
     loaded = true;
-  });
-}
-
-function render() {
-  if (loaded) {
-    n = 1;
   } else {
-    let amount = 0;
-    if (n >= 0 && n < 0.2) { amount = 0.1; }
-    else if (n >= 0.2 && n < 0.5) { amount = 0.04; }
-    else if (n >= 0.5 && n < 0.8) { amount = 0.02; }
-    else if (n >= 0.8 && n < 0.99) { amount = 0.005; }
-    n = Math.min(n + amount, 0.994);
+    window.addEventListener('load', () => {
+      loaded = true;
+    });
   }
-  progress.style.width = n * 100 + "%";
-  if (n >= 1) {
-    clearInterval(render);
-    splash.style.opacity = 0;
-    splash.style.pointerEvents = "none";
-    setTimeout(() => splash.display = "none", 1000);
-  }
-}
 
-setInterval(render, 500);`
+  function render() {
+    if (loaded) {
+      n = 1;
+    } else {
+      let amount = 0;
+      if (n >= 0 && n < 0.2) { amount = 0.1; }
+      else if (n >= 0.2 && n < 0.5) { amount = 0.04; }
+      else if (n >= 0.5 && n < 0.8) { amount = 0.02; }
+      else if (n >= 0.8 && n < 0.99) { amount = 0.005; }
+      n = Math.min(n + amount, 0.994);
+    }
+    progress.style.width = n * 100 + "%";
+    if (n >= 1) {
+      clearInterval(render);
+      splash.style.opacity = 0;
+      splash.style.pointerEvents = "none";
+      setTimeout(() => splash.display = "none", 1000);
+    }
+  }
+
+  setInterval(render, 500);
+}`
         }}
       />
     </div>
