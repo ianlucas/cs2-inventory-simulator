23c4a4c11435f7d07e21251aa00fa25412c35fb5
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-03-13 18:10:22 -0300

    fix: hide splash if extension causes hydration mismatch
    Some browser extensions can mess with the document before React, and that can cause hydration mismatch. React throws a couple of exceptions and mounts the entire client again, so we need to make sure the splash is hidden away when that happens.

--- app/components/splash.tsx
+++ app/components/splash.tsx
@@ -30,7 +30,8 @@ export function Splash() {
         style={{
           border: "1px solid #fff",
           borderRadius: "4px",
-          filter: "drop-shadow(0 0 0.5rem rgba(0, 0, 0, 0.5))"
+          minWidth: "216.859px",
+          minHeight: "44px"
         }}
       >
         <div
@@ -53,45 +54,70 @@ export function Splash() {
           />
         </div>
       </div>
+      <style
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: `:root {
+  color-scheme: dark;
+}`
+        }}
+      />
       <script
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
-          __html: `if (typeof document !== "undefined") {
-  const splash = document.getElementById("splash");
-  const progress = document.getElementById("splash-progress");
-  let n = 0;
-  let loaded = false;
+          __html: `if (!window.g_SplashInitialized) {
+  window.g_SplashInitialized = true;
+  window.g_SplashN = 0;
+  window.g_SplashLoaded = false;
+  window.g_SplashErrors = [];
 
   if (document.readyState === 'complete') {
-    loaded = true;
+    window.g_SplashLoaded = true;
   } else {
     window.addEventListener('load', () => {
-      loaded = true;
+      window.g_SplashLoaded = true;
     });
   }
 
-  function render() {
-    if (loaded) {
-      n = 1;
+  window.g_SplashRender = function () {
+    const splash = document.getElementById("splash");
+    const progress = document.getElementById("splash-progress");
+    if (splash.display === "none") {
+      return;
+    }
+    if (window.g_SplashLoaded) {
+      window.g_SplashN = 1;
     } else {
       let amount = 0;
-      if (n >= 0 && n < 0.2) { amount = 0.1; }
-      else if (n >= 0.2 && n < 0.5) { amount = 0.04; }
-      else if (n >= 0.5 && n < 0.8) { amount = 0.02; }
-      else if (n >= 0.8 && n < 0.99) { amount = 0.005; }
-      n = Math.min(n + amount, 0.994);
+      if (window.g_SplashN >= 0 && window.g_SplashN < 0.2) { amount = 0.1; }
+      else if (window.g_SplashN >= 0.2 && window.g_SplashN < 0.5) { amount = 0.04; }
+      else if (window.g_SplashN >= 0.5 && window.g_SplashN < 0.8) { amount = 0.02; }
+      else if (window.g_SplashN >= 0.8 && window.g_SplashN < 0.99) { amount = 0.005; }
+      window.g_SplashN = Math.min(window.g_SplashN + amount, 0.994);
     }
-    progress.style.width = n * 100 + "%";
-    if (n >= 1) {
-      clearInterval(render);
-      splash.style.opacity = 0;
-      splash.style.pointerEvents = "none";
-      setTimeout(() => splash.display = "none", 1000);
+    if (window.g_SplashN >= 1) {
+      setTimeout(window.g_SplashEnd, 1000);
+    } else {
+      progress.style.width = window.g_SplashN * 100 + "%";
+      setTimeout(window.g_SplashRender, 500);
     }
   }
 
-  setInterval(render, 500);
-}`
+  window.g_SplashEnd = function () {
+    const splash = document.getElementById("splash");
+    const progress = document.getElementById("splash-progress");
+    progress.style.width = window.g_SplashN * 100 + "%";
+    splash.style.opacity = 0;
+    splash.style.pointerEvents = "none";
+    setTimeout(() => splash.display = "none", 1000);
+  }
+
+  window.g_SplashRender();
+}
+
+window.onerror = (event, source, lineno, colno, error) => {
+  window.g_SplashRender();
+};`
         }}
       />
     </div>
