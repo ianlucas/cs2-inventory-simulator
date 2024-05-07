20ed359888684b8884c100a1a88d34b294720959
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-05-07 15:14:58 -0300

    feat: add files for supported system  languages (#72)

--- app/translation.server.ts
+++ app/translation.server.ts
@@ -9,7 +9,33 @@ import { readFileSync, readdirSync } from "fs";
 import { resolve } from "path";
 import { z } from "zod";
 import { brazilian } from "~/translations/brazilian";
+import { bulgarian } from "~/translations/bulgarian";
+import { czech } from "~/translations/czech";
+import { danish } from "~/translations/danish";
+import { dutch } from "~/translations/dutch";
 import { english } from "~/translations/english";
+import { finnish } from "~/translations/finnish";
+import { french } from "~/translations/french";
+import { german } from "~/translations/german";
+import { greek } from "~/translations/greek";
+import { hungarian } from "~/translations/hungarian";
+import { italian } from "~/translations/italian";
+import { japanese } from "~/translations/japanese";
+import { koreana } from "~/translations/koreana";
+import { latam } from "~/translations/latam";
+import { norwegian } from "~/translations/norwegian";
+import { polish } from "~/translations/polish";
+import { portuguese } from "~/translations/portuguese";
+import { romanian } from "~/translations/romanian";
+import { russian } from "~/translations/russian";
+import { schinese } from "~/translations/schinese";
+import { spanish } from "~/translations/spanish";
+import { swedish } from "~/translations/swedish";
+import { tchinese } from "~/translations/tchinese";
+import { thai } from "~/translations/thai";
+import { turkish } from "~/translations/turkish";
+import { ukrainian } from "~/translations/ukrainian";
+import { vietnamese } from "~/translations/vietnamese";
 
 export type SystemTranslations = Record<string, Record<string, string>>;
 
@@ -42,7 +68,33 @@ function readItemTranslations() {
 export function setupTranslation() {
   global.$systemTranslations = {
     brazilian,
-    english
+    bulgarian,
+    czech,
+    danish,
+    dutch,
+    english,
+    finnish,
+    french,
+    german,
+    greek,
+    hungarian,
+    italian,
+    japanese,
+    koreana,
+    latam,
+    norwegian,
+    polish,
+    portuguese,
+    romanian,
+    russian,
+    schinese,
+    spanish,
+    swedish,
+    tchinese,
+    thai,
+    turkish,
+    ukrainian,
+    vietnamese
   };
 
   global.$itemsTranslations = readItemTranslations();
---
+++ app/translations/latam.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ian Lucas. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { english } from "./english";
+
+export const latam = {
+  ...english
+};
