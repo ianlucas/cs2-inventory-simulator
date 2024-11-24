a9af7b7e350dc2c65058113c50e231bd15f945ac
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-11-24 00:43:54 -0300

    refactor: export directly

--- app/localizations/index.ts
+++ app/localizations/index.ts
@@ -3,62 +3,31 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { brazilian } from "~/localizations/brazilian";
-import { bulgarian } from "~/localizations/bulgarian";
-import { czech } from "~/localizations/czech";
-import { danish } from "~/localizations/danish";
-import { dutch } from "~/localizations/dutch";
-import { english } from "~/localizations/english";
-import { finnish } from "~/localizations/finnish";
-import { french } from "~/localizations/french";
-import { german } from "~/localizations/german";
-import { greek } from "~/localizations/greek";
-import { hungarian } from "~/localizations/hungarian";
-import { italian } from "~/localizations/italian";
-import { japanese } from "~/localizations/japanese";
-import { koreana } from "~/localizations/koreana";
-import { latam } from "~/localizations/latam";
-import { norwegian } from "~/localizations/norwegian";
-import { polish } from "~/localizations/polish";
-import { portuguese } from "~/localizations/portuguese";
-import { romanian } from "~/localizations/romanian";
-import { russian } from "~/localizations/russian";
-import { schinese } from "~/localizations/schinese";
-import { spanish } from "~/localizations/spanish";
-import { swedish } from "~/localizations/swedish";
-import { tchinese } from "~/localizations/tchinese";
-import { thai } from "~/localizations/thai";
-import { turkish } from "~/localizations/turkish";
-import { ukrainian } from "~/localizations/ukrainian";
-import { vietnamese } from "~/localizations/vietnamese";
-
-export {
-  brazilian,
-  bulgarian,
-  czech,
-  danish,
-  dutch,
-  english,
-  finnish,
-  french,
-  german,
-  greek,
-  hungarian,
-  italian,
-  japanese,
-  koreana,
-  latam,
-  norwegian,
-  polish,
-  portuguese,
-  romanian,
-  russian,
-  schinese,
-  spanish,
-  swedish,
-  tchinese,
-  thai,
-  turkish,
-  ukrainian,
-  vietnamese
-};
+export { brazilian } from "~/localizations/brazilian";
+export { bulgarian } from "~/localizations/bulgarian";
+export { czech } from "~/localizations/czech";
+export { danish } from "~/localizations/danish";
+export { dutch } from "~/localizations/dutch";
+export { english } from "~/localizations/english";
+export { finnish } from "~/localizations/finnish";
+export { french } from "~/localizations/french";
+export { german } from "~/localizations/german";
+export { greek } from "~/localizations/greek";
+export { hungarian } from "~/localizations/hungarian";
+export { italian } from "~/localizations/italian";
+export { japanese } from "~/localizations/japanese";
+export { koreana } from "~/localizations/koreana";
+export { latam } from "~/localizations/latam";
+export { norwegian } from "~/localizations/norwegian";
+export { polish } from "~/localizations/polish";
+export { portuguese } from "~/localizations/portuguese";
+export { romanian } from "~/localizations/romanian";
+export { russian } from "~/localizations/russian";
+export { schinese } from "~/localizations/schinese";
+export { spanish } from "~/localizations/spanish";
+export { swedish } from "~/localizations/swedish";
+export { tchinese } from "~/localizations/tchinese";
+export { thai } from "~/localizations/thai";
+export { turkish } from "~/localizations/turkish";
+export { ukrainian } from "~/localizations/ukrainian";
+export { vietnamese } from "~/localizations/vietnamese";
