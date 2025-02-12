28708321175b623665f0142bc16b4b39e6cfbd8e
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-02-12 07:28:23 -0300

    feat: add auth error handling (#177)

--- app/root.tsx
+++ app/root.tsx
@@ -15,12 +15,9 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-  useRouteError
+  useLoaderData
 } from "react-router";
 
-import { faFrown } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { findRequestUser } from "./auth.server";
 import { AppProvider } from "./components/app-context";
 import { Background } from "./components/background";
@@ -156,35 +153,4 @@ export default function App() {
   );
 }
 
-export function ErrorBoundary() {
-  const routeError = useRouteError();
-  const error = routeError instanceof Error ? routeError : undefined;
-  return (
-    <html>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Links />
-      </head>
-      <body className="flex h-screen w-screen items-center justify-center bg-blue-500 font-mono text-white">
-        <div className="lg:w-[1024px]">
-          <FontAwesomeIcon icon={faFrown} className="h-16" />
-          <h1 className="mt-4 text-lg font-bold">
-            Inventory Simulator Application Error
-          </h1>
-          <p>
-            An error has occurred.{" "}
-            <a className="underline" href="/">
-              Click here to refresh.
-            </a>
-          </p>
-          {error?.stack !== undefined && (
-            <pre className="relative mt-4 max-h-[128px] overflow-hidden text-sm text-ellipsis after:pointer-events-none after:absolute after:top-0 after:left-0 after:block after:h-full after:w-full after:bg-linear-to-b after:from-transparent after:to-blue-500 after:content-['']">
-              {error.stack}
-            </pre>
-          )}
-        </div>
-      </body>
-    </html>
-  );
-}
+export { ErrorBoundary } from "~/components/error-boundary";
