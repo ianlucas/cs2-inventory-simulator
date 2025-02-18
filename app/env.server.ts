2ca94b04bdbba674676e5b133c65e29a792c68be
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-02-18 13:48:49 -0300

    rework dockerfile

--- app/env.server.ts
+++ app/env.server.ts
@@ -5,25 +5,15 @@
 
 import { assert } from "@ianlucas/cs2-lib";
 import "dotenv/config";
-import { existsSync, readFileSync } from "fs";
-import { resolve } from "path";
-import { trim } from "./utils/misc";
 
 assert(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 export const SESSION_SECRET = process.env.SESSION_SECRET;
-
-export const BUILD_LAST_COMMIT_PATH = resolve(
-  process.cwd(),
-  ".build-last-commit"
-);
-export const BUILD_LAST_COMMIT = existsSync(BUILD_LAST_COMMIT_PATH)
-  ? trim(readFileSync(BUILD_LAST_COMMIT_PATH, "utf-8"))
-  : undefined;
 
 export const {
   ASSETS_BASE_URL,
   CLOUDFLARE_ANALYTICS_TOKEN,
   CS2_CSGO_PATH,
+  SOURCE_COMMIT,
   STEAM_API_KEY,
   STEAM_CALLBACK_URL
 } = process.env;
