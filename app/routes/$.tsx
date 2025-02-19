1ea90a2c1e45ed5ab510f3b2f1aa2ce141204b11
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-02-19 19:29:25 -0300

    fix missing await

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -7,7 +7,7 @@ import { redirect } from "react-router";
 import { middleware } from "~/http.server";
 import type { Route } from "./+types/$";
 
-export function loader({ request }: Route.LoaderArgs) {
-  middleware(request);
+export async function loader({ request }: Route.LoaderArgs) {
+  await middleware(request);
   return redirect("/");
 }
