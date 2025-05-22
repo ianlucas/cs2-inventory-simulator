93bbb16e6ef4e4573753921a22336202653f323c
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-05-22 14:25:13 -0300

    optimize

--- app/components/item-image.tsx
+++ app/components/item-image.tsx
@@ -4,23 +4,66 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { CS2EconomyItem, CS2InventoryItem } from "@ianlucas/cs2-lib";
-import { ComponentProps } from "react";
+import clsx from "clsx";
+import { ComponentProps, useEffect, useState } from "react";
+import { isServerContext } from "~/globals";
 import { cdn } from "~/utils/economy";
+import { noop } from "~/utils/misc";
+import { FillSpinner } from "./fill-spinner";
+
+let cached: string[] = [];
 
 export function ItemImage({
   item,
+  lazy,
   wear,
   ...props
 }: ComponentProps<"img"> & {
   item: CS2EconomyItem | CS2InventoryItem;
+  lazy?: boolean;
   wear?: number;
 }) {
-  return (
-    <img
-      alt={item.name}
-      draggable={false}
-      src={cdn(item.getImage(wear))}
-      {...props}
-    />
+  const url = cdn(item.getImage(wear));
+  const [loaded, setLoaded] = useState(
+    cached.includes(url) || url.includes("steamcommunity")
   );
+
+  useEffect(() => {
+    if (!loaded) {
+      let controller: AbortController | undefined = undefined;
+      function fetchImage() {
+        controller = new AbortController();
+        fetch(url, { signal: controller?.signal })
+          .then(() => {
+            controller = undefined;
+            setLoaded(true);
+            if (!isServerContext) {
+              cached.push(url);
+            }
+          })
+          .catch(noop);
+      }
+      const idx = setTimeout(fetchImage, lazy ? 500 : 1);
+      return () => {
+        clearTimeout(idx);
+        controller?.abort();
+      };
+    }
+  }, [lazy, loaded]);
+
+  if (!loaded) {
+    return (
+      <div
+        {...props}
+        className={clsx(
+          "relative flex items-center justify-center",
+          props.className
+        )}
+      >
+        <FillSpinner className="opacity-50" />
+      </div>
+    );
+  }
+
+  return <img alt={item.name} draggable={false} src={url} {...props} />;
 }
