c58c939c4ef9b41d0d93972a946c73d2dc973efa
Ian Lucas <ianlucas_ferreira@hotmail.com>
2026-03-10 19:10:20 -0300

    Update safer Tailwind canonical classes

--- app/components/context-divider.tsx
+++ app/components/context-divider.tsx
@@ -4,5 +4,5 @@
  *--------------------------------------------------------------------------------------------*/
 
 export function ContextDivider() {
-  return <div className="h-[1px] w-full bg-neutral-700" />;
+  return <div className="h-px w-full bg-neutral-700" />;
 }
