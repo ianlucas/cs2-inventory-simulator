508f1e4a27e753cd484cde329a7efe234ac0acd8
Ian Lucas <ianlucas_ferreira@hotmail.com>
2026-07-13 19:28:40 -0300

    Add CS2 background videos
    Co-Authored-By: crashzk <32937653+crashzk@users.noreply.github.com>

--- app/data/backgrounds.ts
+++ app/data/backgrounds.ts
@@ -4,8 +4,12 @@
  *--------------------------------------------------------------------------------------------*/
 
 export const backgrounds = [
-  { label: "Ancient", value: "ancient" },
-  { label: "Anubis", value: "anubis" },
+  { label: "Ancient 1", value: "ancient" },
+  { label: "Ancient 2", value: "cs2_ancient_t" },
+  { label: "Anubis 1", value: "anubis" },
+  { label: "Anubis 2", value: "cs2_anubis_a" },
+  { label: "Anubis 3", value: "cs2_anubis_ct" },
+  { label: "Anubis 4", value: "cs2_anubis_water" },
   { label: "Apollo", value: "apollo" },
   { label: "Blacksite", value: "blacksite" },
   { label: "Cobblestone", value: "cbble" },
@@ -13,11 +17,16 @@ export const backgrounds = [
   { label: "County", value: "county" },
   { label: "Engage", value: "engage" },
   { label: "Guard", value: "guard" },
+  { label: "Inferno 1", value: "cs2_inferno_a" },
+  { label: "Inferno 2", value: "cs2_inferno_caverna" },
   { label: "Mutiny", value: "mutiny" },
-  { label: "Nuke", value: "nuke" },
-  { label: "Sirocco Night", value: "sirocco_night" },
-  { label: "Sirocco", value: "sirocco" },
+  { label: "Nuke 1", value: "nuke" },
+  { label: "Nuke 2", value: "cs2_nuke_outside" },
+  { label: "Overpass", value: "cs2_overpass_monster" },
+  { label: "Sirocco 1", value: "sirocco" },
+  { label: "Sirocco 2", value: "sirocco_night" },
   { label: "Swamp", value: "swamp" },
+  { label: "Train", value: "cs2_train" },
   { label: "Vertigo", value: "vertigo" }
 ];
 
